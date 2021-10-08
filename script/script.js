9417100bfc5efb4ecc63fc4400226262295a21bb
akoobeey <akmalmamurov99@gmail.com>
2021-10-08 16:12:02 +0500

    akoobeey

---
+++ script/script.js
@@ -0,0 +1,39 @@
+alert('Asallomu aleykum Xush kelibsiz')
+
+
+
+
+let a = +prompt ('Son kiriting')
+, b = +prompt ('darajani kiriting')
+, c = 1
+for (let i = 1; i <= b; i++) {
+    c = a * c
+    
+}
+
+alert(c)
+
+let son = +prompt ('son kiriting')
+, modal = 1
+for (let k = 1; k <= son; k++) {
+    modal *= k
+    
+}
+alert(modal)
+
+
+
+let ism = prompt ('Ismingizni kiriting');
+let yil = +prompt ('Hozirgi yilni kiriting');
+let fff = +prompt ('Tug`ilgan yilingizni kiriting');
+
+ism + (yil - fff) ? alert (`${ism} voyyy ${(yil - fff ) }  Yoshdasiz topdimku` )  : alert (' Nimadur notog`ri');
+
+
+
+
+
+
+
+
+
